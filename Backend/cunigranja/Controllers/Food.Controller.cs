52dd50f0053028f9ee8897b66ad4e0d1bd9ee617
unknown <SENA@ESPAPRCAJFSP197.FormacionSena.RED>
2024-09-09 15:18:16 -0500

    controlador de gabriel

---
+++ Backend/cunigranja/Controllers/Food.Controller.cs
@@ -0,0 +1,63 @@
+﻿using cunigranja.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class AlimentacionController : Controller
+{
+    [HttpPost("CreateAlimentacion")]
+    public IActionResult Create(Alimentacion alimentacion)
+    {
+        try
+        {
+            // Lógica para crear un registro de alimentación
+            return Ok("Alimentación creada exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+
+    [HttpGet("GetAlimentacion")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            // Lógica para obtener un registro de alimentación por id
+            return Ok("Detalles de la alimentación recuperados exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+
+    [HttpPost("UpdateAlimentacion")]
+    public IActionResult Update( Alimentacion alimentacion)
+    {
+        try
+        {
+            // Lógica para actualizar un registro de alimentación
+            return Ok("Alimentación actualizada exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+
+    [HttpDelete("DeleteAlimentacion")]
+    public IActionResult Delete()
+    {
+        try
+        {
+            // Lógica para eliminar un registro de alimentación
+            return Ok("Alimentación eliminada exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+}
---
+++ Backend/cunigranja/Models/Food.Model.cs
@@ -0,0 +1,12 @@
+﻿namespace cunigranja.Models
+{
+    public class Alimentacion
+    {
+        public string Id { get; set; } 
+        public string  NameAlimento { get; set; }
+        public string cantAlimento { get; set; }
+        public string fecAlimento { get; set; }
+        public string HorAlimentacion { get; set; }
+
+    }
+}
