b35190348271785414ab920bd5b8fad85952aa95
LuisSolano953 <luissolanor2022@gmail.com>
2024-10-02 18:36:05 -0500

    Update Food.Model.cs

--- Backend/cunigranja/Models/Food.Model.cs
+++ Backend/cunigranja/Models/Food.Model.cs
@@ -3,10 +3,10 @@
     public class Alimentacion
     {
         public string Id { get; set; } 
-        public string  NameAlimento { get; set; }
-        public string cantAlimento { get; set; }
-        public string fecAlimento { get; set; }
-        public string HorAlimentacion { get; set; }
+        public string  Name_alimento { get; set; }
+        public string cantidad_alimento { get; set; }
+        public string fecha_limento { get; set; }
+        public string Hora_alimentacion { get; set; }
 
     }
 }
