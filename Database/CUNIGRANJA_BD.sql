82b921b3f5d90919c24ecdf28f78135e98e57a59
unknown <SENA@ESPAPRCAJFSP200.FormacionSena.RED>
2024-10-03 10:36:22 -0500

    cambios a la database

---
+++ Database/CUNIGRANJA_BD.sql
@@ -0,0 +1,182 @@
+CREATE DATABASE  IF NOT EXISTS `cunigranja` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cunigranja`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cunigranja
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cage`
+--
+
+DROP TABLE IF EXISTS `cage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cage` (
+  `Id_cage` int NOT NULL,
+  `capacidad_cage` varchar(45) DEFAULT NULL,
+  `tamaño_cage` varchar(45) DEFAULT NULL,
+  `ubicacion_cage` varchar(45) DEFAULT NULL,
+  `ficha_conejo` int DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL,
+  `fecha_salida` date DEFAULT NULL,
+  `raza_conejo` varchar(45) DEFAULT NULL,
+  `sexo_conejo` varchar(45) DEFAULT NULL,
+  `estado_cage` varchar(45) DEFAULT NULL,
+  `edad_conejo` int DEFAULT NULL,
+  PRIMARY KEY (`Id_cage`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cage`
+--
+
+LOCK TABLES `cage` WRITE;
+/*!40000 ALTER TABLE `cage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `food` (
+  `Id_food` int NOT NULL,
+  `name_food` varchar(45) DEFAULT NULL,
+  `cantidad_food` varchar(45) DEFAULT NULL,
+  `fecha_food` date DEFAULT NULL,
+  `hora_food` time DEFAULT NULL,
+  PRIMARY KEY (`Id_food`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `health`
+--
+
+DROP TABLE IF EXISTS `health`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `health` (
+  `idhealth` int NOT NULL,
+  `name_health` varchar(45) DEFAULT NULL,
+  `fecha_health` date DEFAULT NULL,
+  PRIMARY KEY (`idhealth`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `health`
+--
+
+LOCK TABLES `health` WRITE;
+/*!40000 ALTER TABLE `health` DISABLE KEYS */;
+/*!40000 ALTER TABLE `health` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reproduction`
+--
+
+DROP TABLE IF EXISTS `reproduction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reproduction` (
+  `Id_reproduction` int NOT NULL,
+  `fecha__reproduction` date DEFAULT NULL,
+  PRIMARY KEY (`Id_reproduction`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reproduction`
+--
+
+LOCK TABLES `reproduction` WRITE;
+/*!40000 ALTER TABLE `reproduction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reproduction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `responsible`
+--
+
+DROP TABLE IF EXISTS `responsible`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `responsible` (
+  `Id_responsible` int NOT NULL,
+  `name_responsible` varchar(45) DEFAULT NULL,
+  `tipo_responsible` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id_responsible`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `responsible`
+--
+
+LOCK TABLES `responsible` WRITE;
+/*!40000 ALTER TABLE `responsible` DISABLE KEYS */;
+/*!40000 ALTER TABLE `responsible` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `Id_user` int NOT NULL,
+  `password_user` varchar(45) DEFAULT NULL,
+  `name_user` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-02 20:21:14
