026033af784dc7ffd0b63d4d27bf9ad6f77245ff
unknown <SENA@ESPAPRCAJFSP197.FormacionSena.RED>
2024-10-03 09:05:37 -0500

    primer cambios

---
+++ Backend/cunigranja/Models/Food.Model.cs
@@ -0,0 +1,12 @@
+namespace cunigranja.Models
+{
+    public class FoodModel
+    {
+        public string Id { get; set; } 
+        public string  name_food { get; set; }
+        public string cantidad_food { get; set; }
+        public string fecha_food { get; set; }
+        public string hora_food { get; set; }
+
+    }
+}
