7946697a7dd066d936026596af41b2bbe8419c3a
unknown <SENA@ESPAPRCAJFSP055.FormacionSena.RED>
2024-10-03 09:16:52 -0500

    primer cambio controllers

---
+++ Backend/cunigranja/Controllers/Cage.Controller.cs
@@ -0,0 +1,67 @@
+﻿using cunigranja.Functions;
+using cunigranja.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cunigranja.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class CageController : Controller
+    {
+        [HttpPost("CreateCage")]
+        public IActionResult create(CageModel Cage)
+
+        {
+            try
+            {
+                return Ok();
+            }
+                catch (Exception ex)
+                {
+                     return StatusCode(500, ex.ToString()); 
+                
+                }
+        }
+        [HttpGet("GetCage")]
+
+        public  IActionResult Get (int Id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+
+            }
+        }
+
+       [HttpPost("UpdateCage")]
+        public IActionResult Update(int Id,CageModel Cage)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+
+            }
+        }
+        [HttpDelete("DeleteCage")]
+        public IActionResult Delete(int Id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+
+            }
+        }
+    }
+}
---
+++ Backend/cunigranja/Controllers/Responsible.Controller.cs
@@ -0,0 +1,73 @@
+﻿using cunigranja.Functions;
+using cunigranja.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cunigranja.Controllers
+{
+        [ApiController]
+        [Route("Api/[controller]")]
+        public class ResponsibleController : Controller
+        {
+            [HttpPost("CreateResponsible")]
+            public IActionResult create(ResponsibleModel Resposible)
+
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.ToString());
+
+                }
+            }
+            [HttpGet("GetResponsible")]
+
+            public IActionResult Get(int Id)
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.ToString());
+
+                }
+            }
+
+        
+
+        [HttpPost("UpdateResponsible")]
+            public IActionResult Update(int Id, ResponsibleModel Responsible)
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.ToString());
+
+                }
+            }
+            [HttpDelete("DeleteResponsible")]
+            public IActionResult Delete(int Id)
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.ToString());
+
+                }
+            }
+        }
+    }
+        
+    
+
