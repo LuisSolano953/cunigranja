5bae7b5b2056cb7efb1114ba82684292418079c7
unknown <SENA@ESPAPRCAJFSP055.FormacionSena.RED>
2024-10-03 09:21:07 -0500

    segundo cambios models

---
+++ Backend/cunigranja/Models/Cage.Model.cs
@@ -0,0 +1,19 @@
+﻿namespace cunigranja.Models
+{
+    public class CageModel
+    {
+        public int Id_cage{ get; set; }
+        public string capacidad_cage { get; set; }
+        public string tamaño_cage { get; set; }
+        public string ubicacio_cage { get; set; }
+        public int ficha_conejo { get; set; }
+        public string sexo_conejo { get; set; }
+        public DateTime fecha_ingreso { get; set; }
+        public DateTime fecha_salida { get; set; }
+        public string raza_conejo { get; set; }
+        public string estado_cage { get; set; }
+        public int edad_conejo { get; set; }
+
+
+    }
+}
---
+++ Backend/cunigranja/Models/Responsible.Model.cs
@@ -0,0 +1,11 @@
+﻿namespace cunigranja.Models
+{
+    public class ResponsibleModel
+    {
+        public int Id_responsible { get; set; }
+        public string name_responsible { get; set; }
+        public string  tipo_responsible { get; set; }
+  
+
+        }
+}
