b8ce9199eb2d8a8a1b1cd621b6a371951dec399d
LuisSolano953 <luissolanor2022@gmail.com>
2024-10-02 19:46:51 -0500

    Update Cage.Model.cs

--- Backend/cunigranja/Models/Cage.Model.cs
+++ Backend/cunigranja/Models/Cage.Model.cs
@@ -2,17 +2,17 @@
 {
     public class CageModel
     {
-        public int Id_Jaula{ get; set; }
-        public string Cap_Jaula { get; set; }
-        public int Tam_Jaula { get; set; }
-        public string Ubi_Jaula { get; set; }
-        public int Fic_Conejo { get; set; }
-        public string Sex_Jaula { get; set; }
-        public DateTime Fec_Ingreso { get; set; }
-        public DateTime Fec_Salida { get; set; }
-        public string Raz_Conejo { get; set; }
-        public string Est_Jaula { get; set; }
-        public int Edad_Conejo { get; set; }
+        public int Id_jaula{ get; set; }
+        public string capacidad_jaula { get; set; }
+        public string tamaño_jaula { get; set; }
+        public string ubicacio_jaula { get; set; }
+        public int ficha_conejo { get; set; }
+        public string sexo_jaula { get; set; }
+        public DateTime fecha_ingreso { get; set; }
+        public DateTime fecha_salida { get; set; }
+        public string raza_conejo { get; set; }
+        public string estado_jaula { get; set; }
+        public int edad_conejo { get; set; }
 
 
     }
